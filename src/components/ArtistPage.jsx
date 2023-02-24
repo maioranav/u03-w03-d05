5a42e682320771f3d172edc6cf55944ecd774552
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-24 11:10:10 +0100

    fixed class to classname react

--- src/components/ArtistPage.jsx
+++ src/components/ArtistPage.jsx
@@ -4,29 +4,29 @@ import { TopLinks } from "./TopLinks";
 export const ArtistPage = () => {
   return (
     <>
-      <Col class="col-12 col-md-9 offset-md-3 mainPage">
+      <Col className="col-12 col-md-9 offset-md-3 mainPage">
         <TopLinks />
 
         <Row>
           <Col xs={12} md={10} lg={10} className="mt-5">
             <h2 className="titleMain"></h2>
             <div id="followers"></div>
-            <div class="d-flex justify-content-center" id="button-container">
-              <Button class="btn btn-success mr-2 mainButton d-none" id="playButton">
+            <div className="d-flex justify-content-center" id="button-container">
+              <Button className="btn btn-success mr-2 mainButton d-none" id="playButton">
                 PLAY
               </Button>
-              <Button class="btn btn-outline-light mainButton d-none" id="followButton">
+              <Button className="btn btn-outline-light mainButton d-none" id="followButton">
                 FOLLOW
               </Button>
             </div>
           </Col>
         </Row>
-        <Row class="mb-3">
-          <Col xs={10} md={10} lg={10} class="offset-1 p-0">
-            <div class="mt-4 d-flex justify-content-start">
-              <h2 class="text-white font-weight-bold">Tracks</h2>
+        <Row className="mb-3">
+          <Col xs={10} md={10} lg={10} className="offset-1 p-0">
+            <div className="mt-4 d-flex justify-content-start">
+              <h2 className="text-white font-weight-bold">Tracks</h2>
             </div>
-            <div class="pt-5 mb-5">
+            <div className="pt-5 mb-5">
               <Row id="apiLoaded"></Row>
             </div>
           </Col>
